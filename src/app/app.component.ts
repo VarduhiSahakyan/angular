695237a8e6ee717e05ccafe91cc793ae1b99a08e
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-03-15 15:04:56 +0400

    My First Component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,9 @@ import {Component} from "@angular/core";
 @Component({
   selector: 'pm-root',
   template: `
-  <div><h1>{{pageTitle}}</h1>
-    <div>My First Component</div>
-    
-  </div>
+    <div><h1>{{pageTitle}}</h1>
+      <div>My First Component</div>
+    </div>
   `
 })
 export class AppComponent {
