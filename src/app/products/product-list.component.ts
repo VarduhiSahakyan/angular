a4f09089a495e1a737c8a74897530efc5f122441
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-03-22 18:07:55 +0400

    add getter and setter for list filter

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -12,7 +12,16 @@ export class ProductListComponent implements OnInit{
   imageWidth = 50;
   imageMargin = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+  private _listFilter: string = '';
+
+  get listFilter(): string {
+    return  this._listFilter;
+  }
+
+  set listFilter(value: string){
+    this._listFilter =  value;
+    console.log("In setter" , value);
+  }
   products: IProduct[] = [
     {
       "productId": 2,
@@ -61,7 +70,7 @@ export class ProductListComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    console.log("In OnInit");
+    this._listFilter = "cart";
   }
 
 }
