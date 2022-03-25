9e1481a19f4afbf9a10b9090921883b83d43c4eb
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-03-25 18:58:36 +0400

    navigated and routing to welcome page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from "@angular/forms";
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {ProductListComponent} from "./products/product-list.component";
 import {ConvertToSpacePipe} from "./shared/convert-to-space.pipe";
 import {StarComponent} from "./shared/star.component";
 import {HttpClientModule} from "@angular/common/http";
+import {ProductDetailComponent} from './products/product-detail.component';
+import {WelcomeComponent} from "./home/welcome.component";
+import {RouterModule} from "@angular/router";
 // identify the class as an Angular module by attaching
 // the @Ngmodule detector and passing metadata defining
 // the details af this Angular module. the properties
@@ -15,9 +18,11 @@ import {HttpClientModule} from "@angular/common/http";
   // to this module
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
     ConvertToSpacePipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
   ],
   // imports array define the external modules that want
   // to have available to all of the components that belong
@@ -26,11 +31,19 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id', component: ProductDetailComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
 
   // bootstrap array defines the startup component of the app
   //it have to contain the selector used in index.html
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
