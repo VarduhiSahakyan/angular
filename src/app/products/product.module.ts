e0edf4ee62ecb7f47585ab202d69c42e982a72d3
Varduhi Sahakyan <vardsahakyan33@gmail.com>
2022-03-29 13:10:07 +0400

    add sheared.module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {ProductDetailComponent} from "./product-detail.component";
 import {ProductListComponent} from "./product-list.component";
 import {ConvertToSpacePipe} from "../shared/convert-to-space.pipe";
-import {StarComponent} from "../shared/star.component";
-import {FormsModule} from "@angular/forms";
 import {RouterModule} from "@angular/router";
 import {ProductDetailGuard} from "./product-detail.guard";
 import { SharedModule } from '../shared/shared.module';
@@ -14,11 +11,9 @@ import { SharedModule } from '../shared/shared.module';
     ProductListComponent,
     ProductDetailComponent,
     ConvertToSpacePipe,
-    StarComponent
+
   ],
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       {path: 'products', component: ProductListComponent},
       {path: 'products/:id',
